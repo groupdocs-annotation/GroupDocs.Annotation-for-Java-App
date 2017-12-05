6dd9ba1e7084bac8bed0da125a2ac7cf164c6f62
Saqib Masood <saqib@masood.pk>
2017-12-05 12:01:14 +0500

    Filter out system files from list

--- src/main/java/com/groupdocs/ui/servlet/FilesServlet.java
+++ src/main/java/com/groupdocs/ui/servlet/FilesServlet.java
@@ -1,10 +1,6 @@
 package com.groupdocs.ui.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.groupdocs.annotation.domain.AnnotationReplyInfo;
-import com.groupdocs.annotation.domain.results.AddReplyResult;
-import com.groupdocs.annotation.domain.results.GetAnnotationResult;
-import com.groupdocs.annotation.handler.AnnotationImageHandler;
 import com.groupdocs.ui.Utils;
 
 import javax.servlet.ServletException;
@@ -13,7 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 @WebServlet("/files")
@@ -22,10 +19,11 @@ public class FilesServlet extends HttpServlet {
         response.setHeader("Content-Type", "application/json");
 
         ArrayList<String> list = new ArrayList<>();
-        Files.newDirectoryStream(Paths.get(Utils.getStoragePath())).forEach(path -> {
-            if (Files.isRegularFile(path)) {
-                list.add(path.getFileName().toString());
-            }
+        Files.newDirectoryStream(
+                Paths.get(Utils.getStoragePath()),
+                entry -> Files.isRegularFile(entry) && !entry.getFileName().toString().startsWith("GroupDocs.") && !entry.getFileName().toString().equalsIgnoreCase("README.txt")
+        ).forEach(path -> {
+            list.add(path.getFileName().toString());
         });
 
         new ObjectMapper().writeValue(response.getOutputStream(), list);
