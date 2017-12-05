5dbfd38aae5953c16d995a804c42bbe169502255
Saqib Masood <saqib@masood.pk>
2017-12-05 11:59:22 +0500

    Server throws error when no file is selected

--- src/main/webapp/app.controller.pageCanvas.js
+++ src/main/webapp/app.controller.pageCanvas.js
@@ -4,12 +4,19 @@
     function main($rootScope, $scope, AnnotationListFactory, DocumentInfoFactory) {
 
         $rootScope.$watch('selectedFile', function () {
+            if (typeof($rootScope.selectedFile) !== 'string' || !$rootScope.selectedFile.length) {
+                return;
+            }
+
             $rootScope.docInfo = DocumentInfoFactory.get({
                 filename: $rootScope.selectedFile
             });
             $rootScope.annotationsList = AnnotationListFactory.query({
                 filename: $rootScope.selectedFile
             });
+        });
+
+        $rootScope.$watch('selectedFile', function () {
             $rootScope.selectedDrawingTool = 'select';
             $rootScope.selectedAnnotationGuid = null;
         });
