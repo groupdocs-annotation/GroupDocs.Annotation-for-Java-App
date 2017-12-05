76f17e32f3303bc5be69682e65de4462a7bba962
Saqib Masood <saqib@masood.pk>
2017-12-05 12:03:08 +0500

    Specify startup file in URL

--- src/main/webapp/app.run.js
+++ src/main/webapp/app.run.js
@@ -1,8 +1,38 @@
 (function () {
     'use strict';
 
+    var STARTUP_FILE = '';
+
     function main($rootScope) {
-        $rootScope.selectedFile = '';
+        var re = /([^&=]+)=([^&]*)/g;
+        var m, s;
+
+        if (typeof(s) === 'undefined') {
+            while (m = re.exec(location.hash.slice(1))) {
+                if (decodeURIComponent(m[1]) === 'file') {
+                    s = decodeURIComponent(m[2]);
+                    break;
+                }
+            }
+        }
+        if (typeof(s) === 'undefined') {
+            while (m = re.exec(location.search.slice(1))) {
+                if (decodeURIComponent(m[1]) === 'file') {
+                    s = decodeURIComponent(m[2]);
+                    break;
+                }
+            }
+        }
+
+        if (typeof(s) === 'undefined') {
+            if (typeof(STARTUP_FILE) !== 'undefined' && STARTUP_FILE.length) {
+                s = STARTUP_FILE;
+            }
+        }
+
+        if (typeof(s) !== 'undefined') {
+            $rootScope.selectedFile = s;
+        }
     }
 
     angular.module('GroupDocsAnnotationApp').run(main);
